cff0e864d8e8f19cfaa78fbe0fb4bd2d8a3ee4d4
pino0017 <mathias.pinon@etudiant.univ-reims.fr>
2023-06-13 11:52:27 +0200

    Création de la classe people

---
+++ src/Entity/People.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Entity;
+
+class People
+{
+    private int $avatarId;
+    private string $birthday;
+    private string $deathday;
+    private string $name;
+    private string $biography;
+    private string $placeOfBirth;
+    private int $id;
+
+}
