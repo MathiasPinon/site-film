d9584321bee858f267ddf21367347ea6f8f373a5
pino0017 <mathias.pinon@etudiant.univ-reims.fr>
2023-06-13 10:16:40 +0200

    Ajout de la classe MovieCollectiona avec la méthode getAllMovie

---
+++ src/Entity/Collection/MovieCollection.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace Entity\Collection;
+
+use Database\MyPdo;
+use Entity\Movie;
+use Entity\PDO;
+
+class MovieCollection
+{
+    /**
+     * Méthode de classe qui permet de donner tous les films
+     * @return array
+     */
+    public static function getAllMovie():array{
+        $sql = MyPdo::getInstance()->prepare(
+            <<<SQL
+            SELECT posterId , originalLanguage , originalTitle , overview , releaseDate , runtime , tagline , title , id
+            FROM movie 
+        
+SQL );
+        $sql->execute();
+        return $sql->fetchAll(PDO::FETCH_CLASS, Movie::class);
+    }
+
+}
