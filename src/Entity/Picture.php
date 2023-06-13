588a8dff5dd30615ec56dd57664a98668502ce19
pino0017 <mathias.pinon@etudiant.univ-reims.fr>
2023-06-13 14:22:11 +0200

    Création de la classe Picture avec FindById

---
+++ src/Entity/Picture.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace Entity;
+
+use Database\MyPdo;
+
+class Picture
+{
+    private int $id;
+    private string $jpeg ;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getJpeg(): string
+    {
+        return $this->jpeg;
+    }
+
+    /**
+     * @param int $id
+     * @param string $jpeg
+     */
+
+
+
+    public static function FindById(int $idPoster){
+        $sql = MyPdo::getInstance()->prepare(
+            <<<SQL
+                SELECT i.id , i.jpeg 
+                FROM image i
+                INNER JOIN movie m ON (i.id = m.posterId)
+                WHERE m.posterId = :Id 
+SQL
+
+        );
+
+        $sql->execute([':Id'=> $idPoster]);
+        return $sql->fetchAll(\PDO::FETCH_CLASS , Picture::class);
+    }
+}
