56304490a91b11dce6f62645e513626e9a021169
Romain Daunat <romain.daunat@etudiant.univ-reims.fr>
2023-06-14 01:20:13 +0200

    début de l'établissement de la page film.php

--- public/film.php
+++ public/film.php
@@ -1,1 +1,27 @@
 <?php
+
+declare(strict_types=1);
+
+use Database\MyPdo;
+use html\AppWebPage;
+use Entity\Movie;
+use Entity\Collection\PeopleCollection;
+
+$webPage = new AppWebPage();
+$film = new Movie();
+$id = $film->getId();
+
+$webPage->appendContent("<header>".$film->getTitle()."</header><main>");
+
+$webPage->appendContent("<div class='film'>
+                                    <img src='../images/poster_default.png' alt='poster'/>
+                                    <div class='titre'>".$film->getTitle()."</div>".
+                                   "<div class='date'>".$film->getReleaseDate()."</div>".
+                                   "<div class='titreOriginal'>".$film->getOriginalTitle()."</div>".
+                                   "<div class='slogan'>".$film->getTagline()."</div>".
+                                   "<div class='resume'>".$film->getId()."</div></div>");
+
+$webPage->appendContent("</main><footer>".$webPage->getLastModififcation()."</footer>");
+
+echo $webPage->ToHTML();
+
